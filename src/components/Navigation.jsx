5f3d0a323af74893c2a3555b9fb72edc2f789926
Silas Morales <silas.mora22@gmail.com>
2023-08-30 13:30:39 -0700

    attempting to fix page not found netlify

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -18,8 +18,9 @@ const Navigation = ({ showOnFooter, showOnModal, setModalOpen }) => {
       {navigationLinks.map((item, idx) => (
         <ul key={idx}>
           <NavLink className={`${showOnFooter ? 'hover:text-lightCream' : 'hover:text-darkGreyBlue'} ${showOnModal ? 'font-fraunces text-darkGreyBlue text-2xl font-bold leading-8 relative' : 'text-xs text-grey font-semibold tracking-wider uppercase'} transition ease-in-out duration-300`} 
-          to={item.route} 
-          onClick={closeModal}>{item.text}</NavLink>
+          to={item.route}
+          onClick={closeModal}
+          >{item.text}</NavLink>
         </ul>
       ))}
     </div>
