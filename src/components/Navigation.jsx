103bdf7031290166afdd34bf11fbc212ce42d89e
Silas Morales <silas.mora22@gmail.com>
2023-08-30 12:02:41 -0700

    adjusted some js

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,11 +9,17 @@ const Navigation = ({ showOnFooter, showOnModal, setModalOpen }) => {
     {text: 'Create Your Plan', route: '/subscribe'}
   ]
 
+  const closeModal = () => {
+    setModalOpen(false)
+  }
+
   return (
     <div className={`${showOnFooter || showOnModal ? 'flex flex-col gap-4 md:flex-row md:gap-8' : 'hidden md:flex gap-8'}`}>
       {navigationLinks.map((item, idx) => (
         <ul key={idx}>
-          <NavLink className={`${showOnFooter ? 'hover:text-lightCream' : 'hover:text-darkGreyBlue'} ${showOnModal ? 'font-fraunces text-darkGreyBlue text-2xl font-bold leading-8 relative' : 'text-xs text-grey font-semibold tracking-wider uppercase'} transition ease-in-out duration-300`} to={item.route} onClick={() => setModalOpen(false)}>{item.text}</NavLink>
+          <NavLink className={`${showOnFooter ? 'hover:text-lightCream' : 'hover:text-darkGreyBlue'} ${showOnModal ? 'font-fraunces text-darkGreyBlue text-2xl font-bold leading-8 relative' : 'text-xs text-grey font-semibold tracking-wider uppercase'} transition ease-in-out duration-300`} 
+          to={item.route} 
+          onClick={closeModal}>{item.text}</NavLink>
         </ul>
       ))}
     </div>
